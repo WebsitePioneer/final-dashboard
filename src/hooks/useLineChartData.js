c5d1c6e29191e473b851bda8fce4758d6a824de6
WebsitePioneer <vishnuviruz103@gmail.com>
2024-08-26 18:09:52 +0530

    localhost changed

--- src/hooks/useLineChartData.js
+++ src/hooks/useLineChartData.js
@@ -2,7 +2,7 @@ import { useQuery } from 'react-query';
 import axios from 'axios';
 
 const fetchLineChartData = async () => {
-  const response = await axios.get("/api/chart/line", {
+  const response = await axios.get("https://mock-react-dashboard-api.alphaos.workers.dev/chart/line", {
     headers: {
       "Content-Type": "application/json",
     }
--- src/hooks/useTableChartData.js
+++ src/hooks/useTableChartData.js
@@ -2,7 +2,7 @@ import { useQuery } from 'react-query';
 import axios from 'axios';
 
 const fetchPieBarChartData = async () => {
-  const response = await axios.get("/api/chart/table", {
+  const response = await axios.get("https://mock-react-dashboard-api.alphaos.workers.dev/chart/table", {
     headers: {
       "Content-Type": "application/json",
     }
